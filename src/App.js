de2a97d72093495ff676c0743ee9b611dcfbddcf
chami98 <chamikara98@gmail.com>
2022-07-08 14:11:21 +0530

    fetch weather data from weatherapi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [place, setplace] = useState("Colombo");
+  const [placeInfo, setplaceInfo] = useState({});
+
+  const handleFetch = () => {
+    fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=0d50a5b833b84b7bb5080226220807&q=${place}&days=1&aqi=no&alerts=no`
+    )
+      .then((response) => response.json())
+      .then((data) =>
+        setplaceInfo({
+          name: data.location.name,
+          country: data.location.country,
+          farenhiet: {
+            current: data.current.temp_f,
+            high: data.forecast.forecastday[0].day.maxtemp_f,
+            low: data.forecast.forecastday[0].day.mintemp_f,
+          },
+          condition: data.current.condition.text,
+        })
+      );
+  };
+
+  console.log(placeInfo);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search_input">
+        <input
+          type="text"
+          value={place}
+          onChange={(e) => setplace(e.target.value)}
+        ></input>
+
+        <button onClick={() => handleFetch()}>Search</button>
+      </div>
+
+      <div className="weather-container"></div>
     </div>
   );
 }
